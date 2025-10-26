22ea3b1df8510a29ceb8472479bd5bfbf306dffe
rparida103 <44666581+rparida103@users.noreply.github.com>
2025-10-26 20:18:14 +0530

    Add new Python project: Generate a lightweight release notes generator app...

---
+++ release_notes_generator/tests/test_utils.py
@@ -0,0 +1,10 @@
+import unittest
+from utils import format_date
+
+class TestUtils(unittest.TestCase):
+    def test_format_date(self):
+        # Test date formatting
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
